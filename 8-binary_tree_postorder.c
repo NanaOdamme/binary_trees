cb748d59412471af382982a6b7915b84c9fb986d
NanaOdamme <eronastyles@gmail.com>
2024-01-03 01:24:38 -0800

    corrected

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,14 +9,9 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-	return;
+		return;
 
-	/* Traverse left subtree */
 	binary_tree_postorder(tree->left, func);
-
-	/* Traverse right subtree */
 	binary_tree_postorder(tree->right, func);
-
-	/* Visit the current node */
 	func(tree->n);
 }
