54547d75e48382fa540a3516904e878839efa063
NanaOdamme <eronastyles@gmail.com>
2024-01-03 01:45:22 -0800

    updated

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,15 +9,16 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	/* If tree is NULL, it is considered full*/
 	if (tree == NULL)
-	return (1);
-
-	/* Check if the current node has either 0 or 2 children*/
-	if ((tree->left == NULL && tree->right != NULL) ||
-	(tree->left != NULL && tree->right == NULL))
 		return (0);
 
-	/* Recursively check the left and right subtrees*/
-	return (binary_tree_is_full(tree->left) &&
-		binary_tree_is_full(tree->right));
+
+	if (!tree->right && !tree->left)
+		return (1);
+
+	if (tree->right && tree->left)
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+
+	return (0);
 }
 
