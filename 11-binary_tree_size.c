c151148b1a1436584375ddd25c426431fa5e1118
NanaOdamme <eronastyles@gmail.com>
2024-01-03 01:27:56 -0800

    updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,12 +8,10 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);  /* If tree is NULL, return 0*/
+		return (0);
 
-	/* Recursively calculate the size of the left and right subtrees*/
 	size_t left_size = binary_tree_size(tree->left);
 	size_t right_size = binary_tree_size(tree->right);
 
-	/* Return the total size of the tree (number of nodes)*/
 	return (left_size + right_size + 1);
 }
